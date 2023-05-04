ff98fcbb533f23cc3bdf7bef12fe8352a4500587
riceyan <johndoe@example.com>
2023-05-04 11:06:46 +0800

    section C commit

--- app/src/main/java/sg/edu/rp/c346/id21017809/week03/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21017809/week03/MainActivity.java
@@ -3,12 +3,37 @@ package sg.edu.rp.c346.id21017809.week03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvDisplay;
+    Button btnDisplay;
+    EditText etInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvDisplay = findViewById(R.id.textViewDisplay);
+        btnDisplay = findViewById(R.id.buttonDisplay);
+        etInput = findViewById(R.id.editTextInput);
+
+        btnDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code for the action
+                etInput.getText().toString();
+                String stringResponse = etInput.getText().toString();
+                tvDisplay.setText(stringResponse);
+
+            }
+        });
+
     }
+
 }
